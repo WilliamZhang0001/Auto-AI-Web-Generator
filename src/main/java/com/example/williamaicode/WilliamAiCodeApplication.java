9269d0b5d13f4684409675a55c7dcf02d2b66578
williamzhang <yifanzhang0920@gmail.com>
2025-09-04 14:29:57 +1000

    Initial commit

---
+++ src/main/java/com/example/williamaicode/WilliamAiCodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.williamaicode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WilliamAiCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WilliamAiCodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/williamaicode/WilliamAiCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.williamaicode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WilliamAiCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
