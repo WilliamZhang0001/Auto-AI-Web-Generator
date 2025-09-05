70751cec898bbb0f85c138ddb0f9a2243ea27b8a
williamzhang <yifanzhang0920@gmail.com>
2025-09-05 23:32:46 +1000

    前端更新

--- auto-ai-generater-frontend/openapi2ts.config.ts
+++ auto-ai-generater-frontend/openapi2ts.config.ts
@@ -1,6 +1,13 @@
 // 根据后端接口生成前端请求和 TS 模型代码
+/*
 export default {
   requestLibPath: "import request from '@/request'",
   schemaPath: 'http://localhost:8123/api/v3/api-docs', // 使用本地文件，避免网络依赖
   serversPath: './src',
+}
+*/
+
+export default {
+  schemaPath: 'https://petstore.swagger.io/v2/swagger.json',
+  serversPath: './servers',
 }
