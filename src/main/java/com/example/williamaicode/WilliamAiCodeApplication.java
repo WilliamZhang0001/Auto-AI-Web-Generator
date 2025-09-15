da37a112c799428b3b67704352f01c8114bf8d78
williamzhang <yifanzhang0920@gmail.com>
2025-09-15 22:28:10 +1000

    更新四：用户模块后端 1.开发flex代码生成器； 2,开发核心功能，用户注册、用户登录、获取登录用户信息、用户注册等； 3.全局权限校验AOP； 4.开发管理员增删改查接口； 5.自定义配置解决丢失精度问题；

--- src/main/java/com/example/williamaicode/WilliamAiCodeApplication.java
+++ src/main/java/com/example/williamaicode/WilliamAiCodeApplication.java
@@ -1,9 +1,12 @@
 package com.example.williamaicode;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.example.williamaicode.mapper")
+
 public class WilliamAiCodeApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/example/williamaicode/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.example.williamaicode.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.example.williamaicode.model.entity.User;
+
+/**
+ * 用户 映射层。
+ *
+ * @author <a href="https://github.com/WilliamZhang0001">William张</a>
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
